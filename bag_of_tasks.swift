54479d1a75ecee40aa63163938a2479894659b79
Yadu Nand B <yadudoc1729@gmail.com>
2015-11-20 19:30:49 +0000

    Adding new app remote_shuf.

--- bag_of_tasks.swift
+++ bag_of_tasks.swift
@@ -5,11 +5,17 @@ app (file out) remote_driver (int time)
    sleep time stdout=@out;
 }
 
+app (file out) remote_shuf (int time)
+{
+   shuf "-i 1-100" stdout=@out;
+}
+
 file driver_out[] <simple_mapper; prefix="date", suffix=".out">;
 
-int count = toInt(arg("N", "128"));
-int sleep = toInt(arg("sleep", "900"));
+int count = toInt(arg("N", "1"));
+int sleep = toInt(arg("sleep", "100"));
 
 foreach i in [1:count] {
-    driver_out[i] = remote_driver(sleep);
+    //driver_out[i] = remote_driver(sleep);
+    driver_out[i] = remote_shuf(sleep);
 }
