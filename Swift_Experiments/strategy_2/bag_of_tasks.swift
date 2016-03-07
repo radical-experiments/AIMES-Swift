f42f5d4a3cd0e7d89de959d2f3823d1372e0d5a6
Matteo Turilli <matteo.turilli@gmail.com>
2016-03-07 22:02:09 +0100

    Raw and failed data
    Former-commit-id: fd872820505b90eff69c46b535f35721abe4210d

---
+++ Swift_Experiments/strategy_2/bag_of_tasks.swift
@@ -0,0 +1,22 @@
+type file;
+
+app remote_driver (int time)
+{
+    sleep time;
+}
+
+app (file out) remote_shuf (int time)
+{
+   shuf "-i 1-100" stdout=@out;
+}
+
+//file driver_out[] <simple_mapper; prefix="date", suffix=".out">;
+
+int count = toInt(arg("N", "1"));
+int sleep = toInt(arg("sleep", "100"));
+
+foreach i in [1:count] {
+    //driver_out[i] = remote_driver(sleep);
+    //driver_out[i] = remote_shuf(sleep);
+    remote_driver(sleep);
+}
