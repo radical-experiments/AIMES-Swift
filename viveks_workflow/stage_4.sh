fd828961b277687d07d0c9247444bd42c42b9922
Andre Merzky <andre@merzky.net>
2016-05-04 14:47:16 +0200

    add README for stampede/synapse execution

--- viveks_workflow/stage_4.sh
+++ viveks_workflow/stage_4.sh
@@ -6,9 +6,11 @@ OUTPUT_3_1_I=$3
 
 OUTPUT_4_1_I="output_4_1_$IDX.txt"
 
-cat "$INPUT_SHARED_1_5"  > "$OUTPUT_4_1_I";
-cat "$OUTPUT_3_1_I"     >> "$OUTPUT_4_1_I";
-echo "4:$IDX"           >> "$OUTPUT_4_1_I"
+cat  $INPUT_SHARED_1_5 >> $OUTPUT_4_1_I
+cat  $OUTPUT_3_1_I     >> $OUTPUT_4_1_I
+echo "4:$IDX"          >> $OUTPUT_4_1_I
 
-# sleep 2
+. $HOME/bin.rp/ve/bin/activate
+
+radical-synapse-sample -f 10000000000
 
