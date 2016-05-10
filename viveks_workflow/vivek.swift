3d09f0cdc66a47b1f1a38d606c529ef192a943f7
Andre Merzky <andre@merzky.net>
2016-05-10 22:33:02 +0200

    support chunking in stage 3
    Former-commit-id: 10e6b654f8260f4ebf3ee57ed2777281f49e43c1

--- viveks_workflow/vivek.swift
+++ viveks_workflow/vivek.swift
@@ -1,8 +1,11 @@
 
 type file;
 
-# 64 ... 2048
-int N = toInt(arg("N", "64"));
+int N = toInt(arg("N", "64")); # 64 ... 2048
+int chunk_size = 8; # chunksize for stage 3
+int n_chunks   = 8; # number of chunks
+
+int verb = 1;
 
 # -----------------------------------------------------------------------------
 #
@@ -17,11 +20,11 @@ app (file output_1_1_i,
                                  file input_shared_1_4,
                                  file input_shared_1_5)
 {
-    stage_1_exe i filename(input_shared_1_1) 
-                  filename(input_shared_1_2) 
-                  filename(input_shared_1_3) 
-                  filename(input_shared_1_4) 
-                  filename(input_shared_1_5);
+   stage_1_exe i filename(input_shared_1_1) 
+                 filename(input_shared_1_2) 
+                 filename(input_shared_1_3) 
+                 filename(input_shared_1_4) 
+                 filename(input_shared_1_5);
 }
 
 file input_shared_1_1 <"input_shared_1_1.txt">;
@@ -39,6 +42,15 @@ foreach i in [1:N] {
     file output_1_2_i <single_file_mapper; file=strcat("output_1_2_",i,".txt")>;
     file output_1_3_i <single_file_mapper; file=strcat("output_1_3_",i,".txt")>;
 
+    if (verb == 1) {
+      # trace("stage 1");
+      # trace(filename(input_shared_1_1));
+      # trace(filename(input_shared_1_2));
+      # trace(filename(input_shared_1_3));
+      # trace(filename(input_shared_1_4));
+      # trace(filename(input_shared_1_5));
+    }
+
     (output_1_1_i, 
      output_1_2_i, 
      output_1_3_i) = stage_1(i, 
@@ -51,6 +63,7 @@ foreach i in [1:N] {
     output_1_1[i] = output_1_1_i;
     output_1_2[i] = output_1_2_i;
     output_1_3[i] = output_1_3_i;
+    
 }
 
 
@@ -83,6 +96,12 @@ foreach i in [1:N] {
     file output_2_3_i <single_file_mapper; file=strcat("output_2_3_",i,".txt")>;
     file output_2_4_i <single_file_mapper; file=strcat("output_2_4_",i,".txt")>;
     
+    if (verb == 1) {
+      # trace("stage 2");
+      # trace(filename(input_shared_1_3));
+      # trace(filename(input_shared_1_4));
+      # trace(filename(output_1_1[i]));
+    }
     (output_2_1_i, 
      output_2_2_i, 
      output_2_3_i, 
@@ -102,22 +121,68 @@ foreach i in [1:N] {
 #
 # Stage 3
 #
-app (file[] output_3_1) stage_3 (file   input_shared_1_3, 
+app (file[] output_3_1) stage_3 (int    chunk, 
+                                 int    chunksize, 
+                                 file   input_shared_1_3, 
                                  file[] output_2_2)
 {
     # N cores
-    stage_3_exe filename(input_shared_1_3) 
+    stage_3_exe chunk 
+                chunksize
+                filename(input_shared_1_3) 
                 @output_2_2;
 }
 
-string[] output_3_1_s;
-foreach i in [1:N] {
-    output_3_1_s[i] = strcat("output_3_1_",i,".txt");
-}
-file[] output_3_1 <array_mapper; files=output_3_1_s>;
+# we run stage_3 in chunks of C cores each, so we nee dto subdivide the file
+# list into such chunks
 
-output_3_1 = stage_3(input_shared_1_3,
-                     output_2_2);
+# string[] output_3_1_s;   # output files for all chunks
+# foreach i in [1:N] {
+#     output_3_1_s[i] = strcat("output_3_1_",i,".txt");
+# }
+file[] output_3_1;
+
+# over all chunks
+foreach c in [0:(n_chunks-1)] {
+
+    # file lists for chunk 
+    file[]   output_2_2_c;     # input files for this chunk
+    string[] output_3_1_c_s;   # output files for this chunk
+
+    # over all chunk elements
+    foreach i in [1:chunk_size] {
+
+        # global index
+        int j = c*chunk_size + i;
+        output_2_2_c[i]   = output_2_2[j];
+        output_3_1_c_s[i] = strcat("output_3_1_",j,".txt");
+    }
+
+    # convert into file sets
+    file[] output_3_1_c <array_mapper; files=output_3_1_c_s>;
+
+    # run this chunk
+    if (verb == 1) {
+        string tmp = sprintf("stage 3: %s : %s : %s -> %s", c, 
+                             filename(input_shared_1_3), 
+                             strjoin(output_2_2_c, " "), 
+                             strjoin(output_3_1_c_s, " "));
+        trace(tmp);
+
+    }
+    output_3_1_c = stage_3(c, chunk_size, 
+                           input_shared_1_3,
+                           output_2_2_c);
+
+    # now merge the received files from the chunk into the global thing
+    foreach i in [1:chunk_size] {
+
+        # global index
+        int j = c*chunk_size + i;
+        output_3_1[j] = output_3_1_c[i];
+    }
+    
+}
 
 
 # -----------------------------------------------------------------------------
@@ -131,9 +196,19 @@ app (file output_4_1) stage_4 (file   input_shared_1_5,
     stage_4_exe filename(input_shared_1_5) 
                 @output_3_1;
 }
-file output_4_1  <"output_4_1.txt">;
-output_4_1  = stage_4(input_shared_1_5,
-                      output_3_1);
+
+if (1 == 1) {
+
+    if (verb == 1) {
+        trace("stage 4");
+        trace(filename(input_shared_1_5));
+        trace(@output_3_1);
+    }
+
+    file output_4_1  <"output_4_1.txt">;
+    output_4_1  = stage_4(input_shared_1_5,
+                          output_3_1);
+}
 
 # -----------------------------------------------------------------------------
 
