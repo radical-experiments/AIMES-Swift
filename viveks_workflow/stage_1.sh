83a260e3bf5466bd65c988df0b99a3ec6822675c
Andre Merzky <andre@merzky.net>
2016-05-13 23:19:59 +0200

    toward synapse emulation

--- viveks_workflow/stage_1.sh
+++ viveks_workflow/stage_1.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 IDX=$1
 INPUT_SHARED_1_1=$2
@@ -15,7 +15,30 @@ cat $INPUT_SHARED_1_1 >> $OUTPUT_1_1_I; echo "1:$IDX" >> $OUTPUT_1_1_I
 cat $INPUT_SHARED_1_2 >> $OUTPUT_1_2_I; echo "1:$IDX" >> $OUTPUT_1_2_I
 cat $INPUT_SHARED_1_3 >> $OUTPUT_1_3_I; echo "1:$IDX" >> $OUTPUT_1_3_I
 
+
+# pattern to select suitable profiles
+regex_1='.*sander.MPI -O -i mdshort.in'
+regex_2='.*sander.MPI -O -i min.in'
+regex_3='pyCoCo'
+regex_4='python postexec.py .*'
+
+regex="$regex_1"
+
+# select and count profiles
+profiles=`grep -le "$regex" $HOME/bin.rp/samples/*.prof`
+n_profiles=`echo $profiles | wc -w`
+
+# select a random profile
+n_random=`awk "BEGIN{srand();print int($n_profiles * rand()) + 1;}"`
+profile=`for profile in $profiles; do echo $profile; done | head -n $n_random | tail -n 1`
+samples="${profile%.prof}.json"
+
+test -f "$samples" || echo "$samples does not exist"
+test -f "$samples" || exit 1
+
+echo "sampling $samples"
+
 . $HOME/bin.rp/ve/bin/activate
 
-radical-synapse-sample -f 10000000000
+radical-synapse-emulate -i "$samples"
 
--- viveks_workflow/stage_2.sh
+++ viveks_workflow/stage_2.sh
@@ -15,8 +15,30 @@ cat $INPUT_SHARED_1_4 >> $OUTPUT_2_2_I; echo "2:$IDX" >> $OUTPUT_2_2_I
 cat $OUTPUT_1_1_I     >> $OUTPUT_2_3_I; echo "2:$IDX" >> $OUTPUT_2_3_I
 cat $OUTPUT_1_1_I     >> $OUTPUT_2_4_I; echo "2:$IDX" >> $OUTPUT_2_4_I
 
+
+# pattern to select suitable profiles
+regex_1='.*sander.MPI -O -i mdshort.in'
+regex_2='.*sander.MPI -O -i min.in'
+regex_3='pyCoCo'
+regex_4='python postexec.py .*'
+
+regex="$regex_2"
+
+# select and count profiles
+profiles=`grep -le "$regex" $HOME/bin.rp/samples/*.prof`
+n_profiles=`echo $profiles | wc -w`
+
+# select a random profile
+n_random=`awk "BEGIN{srand();print int($n_profiles * rand()) + 1;}"`
+profile=`for profile in $profiles; do echo $profile; done | head -n $n_random | tail -n 1`
+samples="${profile%.prof}.json"
+
+test -f "$samples" || echo "$samples does not exist"
+test -f "$samples" || exit 1
+
+echo "sampling $samples"
+
 . $HOME/bin.rp/ve/bin/activate
 
-radical-synapse-sample -f 10000000000
-
+radical-synapse-emulate -i "$samples"
 
