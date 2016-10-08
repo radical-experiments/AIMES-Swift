f29da92394777e9dc6fc1a559e8b1c70b13c0fcc
Andre Merzky <andre@merzky.net>
2016-10-09 00:37:05 +0200

    iteration on experiment flow

--- viveks_workflow/templates/runme.sh
+++ viveks_workflow/templates/runme.sh
@@ -13,7 +13,23 @@ export AIMES_SWIFT_VERBOSE=DEBUG
 export AIMES_SWIFT_LOG_TGT=sa.log
 export SAGA_PTY_SSH_TIMEOUT=120
 
-aimes-emgr-rest2 experiment.json 2>&1 > rest.log  & echo $! > rest.pid
-swift            vivek.swift     2>&1 > swift.log & echo $! > swift.pid
-tail -f rest.log
+aimes-emgr-rest2 experiment.json 2>&1 > rest.log  & AIMES_PID=$!
+swift            vivek.swift     2>&1 > swift.log & SWIFT_PID=$!
+
+echo $AIMES_PID > rest.pid
+echo $SWIFT_PID > swift.pid
+
+echo "waiting on $SWIFT_PID"
+wait $SWIFT_PID
+tail swift.log
+
+echo 'fetching profiles'
+SESSION_IDS=`grep 'new session' rest.log | cut -f 5 -d '[' | cut -f 1 -d ']'`
+for sid in $SESSION_IDS
+do
+  radicalpilot-fetch-profiles $sid
+  radicalpilot-fetch-json     $sid
+done
+
+echo 'all done'
 
